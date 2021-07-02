d72cf53ef879f52a54e6425e875c5a299545a93f
mkcyoung <m.c.young0@gmail.com>
2021-07-02 14:03:35 -0600

    exercises 3.5 and 3.6

--- index.js
+++ index.js
@@ -24,38 +24,78 @@ let persons = [
     }
   ]
 
+// Activates express's json parser
+app.use(express.json())
+
 // return all persons
 app.get('/api/persons', (request, response) => {
-response.json(persons)
+    response.json(persons)
 })
 
 // return info for page
 app.get('/info', (request, response) => {
-let head = `<p>Phonebook has info for ${persons.length} people</p>`
-let info = head.concat(`<p> ${new Date()} </p>`)
-response.send(info)
+    let head = `<p>Phonebook has info for ${persons.length} people</p>`
+    let info = head.concat(`<p> ${new Date()} </p>`)
+    response.send(info)
 })
 
 // return individual person
 app.get('/api/persons/:id', (request, response) => {
-const id = Number(request.params.id)
-const person = persons.find(person => person.id === id)
-if (person) {
-    response.json(person)
-} else {
-    response.status(404).end()
-}
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
 // delete person
 app.delete('/api/persons/:id', (request, response) => {
-const id = Number(request.params.id)
-persons = persons.filter(person => person.id !== id)
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
 
-response.status(204).end()
+    response.status(204).end()
+})
+
+// helper function for generating random int for id
+getRandomId = (max) => Math.floor(Math.random() * max)
+
+// Add entry to phonebook
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    // Error handling (TODO: make more succinct)
+    if (!body.name){
+        return response.status(400).json({
+            error: 'missing name'
+        })
+    }
+    else if (!body.number){
+        return response.status(400).json({
+            error: 'missing number'
+        })
+    }
+    else if (persons.map(person => person.name).includes(body.name)){
+        return response.status(400).json({
+            error: 'name already in phonebook'
+        })
+    }
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        // Number needs to be big enough to make collisions unlikely
+        id: getRandomId(100000) 
+    }
+
+    persons = persons.concat(person)
+
+    response.json(persons)
+
 })
 
 const PORT = 3001
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
 })
