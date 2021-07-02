d99b1a901ea0f484c88eb1a7919be66609bbc1f9
mkcyoung <m.c.young0@gmail.com>
2021-07-02 11:50:04 -0600

    exercise 3.3

--- index.js
+++ index.js
@@ -33,7 +33,18 @@ let persons = [
     let info = head.concat(`<p> ${new Date()} </p>`)
     response.send(info)
   })
-  
+
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
