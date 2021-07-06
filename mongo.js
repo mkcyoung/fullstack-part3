c05d21613fda71891e02d1a1cc349e62543d06dd
mkcyoung <m.c.young0@gmail.com>
2021-07-05 19:21:34 -0600

    exercise 3.12 changes

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
+if (process.argv.length < 3 || (process.argv.length > 3 && process.argv.length < 5) ) {
+    console.log('Please provide the password, name, and number as an argument: node mongo.js <password> <name> <number>')
     process.exit(1)
   }
 
@@ -9,7 +9,7 @@ const password = process.argv[2]
 
 const name = process.argv[3]
 
-const number = process.argv[4] // convert to string maybe
+const number = process.argv[4] 
 
 
 const url = 
@@ -25,22 +25,24 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-// Creating a new person
-const person = new Person({
-    name: name,
-    number: number
-})
-
 // Prints all entries in phonebook if no name and number are passed
 if (!name && !number){
     Person.find({}).then(result => {
+        console.log("phonebook:")
         result.forEach(person => {
-          console.log(person)
+          console.log(`${person.name} ${person.number}`)
         })
         mongoose.connection.close()
+        process.exit(0) //Exits out
       })
 }
 else{
+    // Creating a new person
+    const person = new Person({
+        name: name,
+        number: number
+    })
+
     person.save().then(result => {
         console.log(`added ${name} number ${number} to phonebook`)
         mongoose.connection.close()
