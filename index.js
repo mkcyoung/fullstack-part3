867ca277bd68295d25d92226da5cf279a8fd416c
mkcyoung <m.c.young0@gmail.com>
2021-07-30 19:11:41 -0600

    exercise 3.20

--- index.js
+++ index.js
@@ -142,13 +142,14 @@ app.post('/api/persons', (request, response, next) => {
 // adjusting number if name is already in the phonebook
 app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
+    console.log(body)
 
     const person = {
         name: body.name,
         number: body.number
     }
 
-    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    Person.findByIdAndUpdate(request.params.id, person, {new: true, runValidators: true, context: 'query'})
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -157,11 +158,10 @@ app.put('/api/persons/:id', (request, response, next) => {
 
 // Error handling etc.
 const errorHandler = (error, request, response, next) => {
-    console.error("in error handling",error.message)
     if (error.name === "CastError") {
         return response.status(400).send({ error: 'malformatted id' })
     } else if (error.name === "ValidationError") {
-        return response.status(400).json({ error: error.message })
+        return response.status(400).send({ error: error.message })
     }
     next(error)
 }
